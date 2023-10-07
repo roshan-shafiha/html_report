17fefe259448c073fba2a9a9940b20117d6e270f
roshan-shafiha <roshan.shafiha@eaglegenomics.com>
2023-10-07 08:32:20 +0100

    adding files for html report generation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# html_report
+## Html_report
 Generating html report template to import result table and have a standard report for projects
+
+-create a folder to store the template format of html.
+-create a python script with the variables to be modified within the html template.
+-style your html file with CSS.
---
+++ edit_html_template.py
@@ -0,0 +1,22 @@
+# Import libraries
+import pandas as pd
+from jinja2 import Environment, FileSystemLoader
+# Load the data 
+OTU = pd.read_csv('OTU_table.csv',header=(0))
+
+
+# load the env where the template is present 
+env = Environment(loader=FileSystemLoader('template_format'))
+
+# Load the template file
+template = env.get_template('template.html')
+
+# 4. Render the template with variables
+html = template.render(page_title_text='Report',
+                       content_1='Content 1',
+                       content_2= 'Content 2',
+                       otu_table=OTU)
+
+# 5. Write the template to an HTML file
+with open('html_report_output.html', 'w') as f:
+    f.write(html)
